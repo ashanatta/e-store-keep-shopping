683cfe1652fc88bac1f9b2438e7960d00c5910db
ashanatta <muhammadashan1714@gmail.com>
2026-02-17 11:38:44 +0500

    add men,shop,women and header view and install bootstrap

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,10 +3,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // Import your components
 import Home from '../views/Home.vue'
+import Men from '@/views/Men.vue'
+import Women from '@/views/Women.vue'
+import Shop from '@/views/Shop.vue'
 
 // Define routes
 const routes = [
   { path: '/', name: 'Home', component: Home },
+  { path: '/men', name: 'Men', component: Men },
+  { path: '/women', name: 'women', component: Women },
+  { path: '/shop', name: 'shop', component: Shop },
 ]
 
 // Create router
--- frontend/src/views/Women.vue
+++ frontend/src/views/Women.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container mt-4">
-    <h2>Welcome to Home Page</h2>
+    <h2>Welcome to Women Page</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Women'
 }
 </script>
