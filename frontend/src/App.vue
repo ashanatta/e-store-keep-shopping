7e836c90f3615ba1c4c5859431eb0697b3b901d3
ashanatta <muhammadashan1714@gmail.com>
2026-06-22 10:34:43 +0500

    weekness

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="d-flex flex-column min-vh-100">
     <Header />
-       <main class="flex-fill">
+    <main class="flex-fill">
       <RouterView />
     </main>
-      <Footer />
-      <ChatWidget v-if="!isAdmin" />
+    <Footer />
+    <ChatWidget v-if="!isAdmin" />
   </div>
 </template>
 
 <script setup>
-import { useAuth } from '@/composables/useAuth'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import ChatWidget from './components/common/ChatWidget.vue'
+import { useAuth } from "@/composables/useAuth";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import ChatWidget from "./components/common/ChatWidget.vue";
 
-const { isAdmin } = useAuth()
+const { isAdmin } = useAuth();
 </script>
